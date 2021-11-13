372d25180b6668779c8b236bbac4bb7094ea10cc
FabianaFerrara <59937454+FabianaFerrara@users.noreply.github.com>
2021-11-13 19:37:51 +0100

    added button Start and the background

--- MeteorStorm/BreathingView.swift
+++ MeteorStorm/BreathingView.swift
@@ -10,16 +10,34 @@ import Foundation
 struct BreathingView: View {
     var body: some View {
         VStack{
+            Spacer()
             Image(systemName: "lungs")
                 .resizable(resizingMode: .tile)
                 .aspectRatio(contentMode: .fit)
                 .foregroundColor(Color.purple)
                 .frame(width: 123, height: 86)
-                
-            Text("Sit on a chair that supports your back, or lay on a yoga mat on the floor.")
+           
+
+            
+            Text("Sit on a chair that supports your back or lay    \non a yoga  mat on the floor.")
+
+                .padding(.vertical)
             
             Text("Breath in through your nose and out through your mouth.")
+           
+            Spacer()
             
+            Text("Ready?").font(.largeTitle).fontWeight(.bold).padding()
+            Button(action: {
+                
+            }, label: {Text("Start")
+                    .padding(.vertical)
+                    .frame( maxWidth: .infinity)
+
+                .foregroundColor(Color.white)}
+            )
+                .background(Color.purple).cornerRadius(14)
+                .padding(.horizontal)
                 
         }
         
