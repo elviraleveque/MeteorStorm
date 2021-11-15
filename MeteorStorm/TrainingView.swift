0e383d26a3327f00d6139cfaee9bb474e82d06c8
elviraleveque <39759385+elviraleveque@users.noreply.github.com>
2021-11-15 20:55:55 +0100

    Training screen

--- MeteorStorm/TrainingView.swift
+++ MeteorStorm/TrainingView.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+struct TrainingListItem: View {
+    var icon: String = "person"
+    var exercise: String
+    var isCompleted: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(Color(.systemIndigo))
+                .frame(width: 25)
+            VStack (alignment: .leading){
+                Text(exercise)
+                    .font(.headline)
+                Text(isCompleted)
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+            }
+            .padding(.horizontal)
+
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundColor(Color(.systemIndigo))
+        }
+    }
+}
+
 struct TrainingView: View {
     var body: some View {
-        Text("Training View")
+        NavigationView {
+            List{
+                Section(header: Text("Exercises")) {
+                    TrainingListItem(icon: "lungs", exercise: "Breathing", isCompleted: "Completed")
+                    TrainingListItem(icon: "heart.text.square", exercise: "Relationship Caring", isCompleted: "Completed")
+                    TrainingListItem(icon: "eyes", exercise: "Eye Contact", isCompleted: "Completed")
+                    TrainingListItem(icon: "aqi.medium", exercise: "Guided meditation", isCompleted: "Completed")
+                    TrainingListItem(icon: "figure.wave", exercise: "Self Esteem", isCompleted: "Completed")
+                    TrainingListItem(icon: "lungs", exercise: "Traffic Light", isCompleted: "Completed")
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Training")
+        }
     }
 }
 
