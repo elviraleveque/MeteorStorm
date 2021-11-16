dbfa063cf4c77c455fbafea98566ebbe52f4b468
FabianaFerrara <59937454+FabianaFerrara@users.noreply.github.com>
2021-11-16 22:44:20 +0100

    deleted one of the struct, now is in a separate file

--- MeteorStorm/BreathingView.swift
+++ MeteorStorm/BreathingView.swift
@@ -57,42 +57,6 @@ struct BreathingView: View {
           }
 }
 
-
-struct BreathingExercise: View {
-          @State var isVisible = false
-          @State var currentDate = Date()
-          @State var isActive = true
-          
-          var body: some View{
-                    NavigationView{
-                              VStack{
-                                        TextSwitch()
-                                        //
-                                        Spacer()
-                                        Spacer()
-                                        ZStack {
-                                                  Image("MovingCircle")
-                                                            .scaleEffect(isVisible ? 1.0 : 1.9)
-                                                            .onAppear(perform: {
-                                                                      if(isActive) { withAnimation(.easeInOut(duration: 4).repeatCount(16, autoreverses: true))
-                                                                                {
-                                                                                          self.isVisible.toggle()
-                                                                                }
-                                                                      }
-                                                            })//onAppear
-                                                  
-                                                  Image("StaticCircle")
-                                        } //ZStack
-                                        Spacer()
-                                        Spacer()
-                                        CountDown(isActive: $isActive)
-                                        Spacer()
-                                        
-                              }//VsTack
-                    }.navigationTitle("Breathing")
-          }
-}//view with the real exercise
-
 func leadingZero(_ n:Int) -> String {
           return n < 10 ? "0\(n)" : "\(n)"
 }
